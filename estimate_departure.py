b3573632e5ab2b2e6ed6a4c2fb61bab91bd1366f
Dapacr <davidcruz72@gmail.com>
2018-03-26 09:34:12 -0700

    Intial commit.

---
+++ estimate_departure.py
@@ -0,0 +1,42 @@
+# Estimate travel departure time using Google APIs
+
+import googlemaps
+from datetime import datetime, timedelta
+
+def get_directions(origin, destination, time):
+    return gmaps.directions(origin, destination, mode="driving", departure_time=time)
+
+def get_duration(directions, time):
+    duration = directions[0]["legs"][0]["duration_in_traffic"]["value"]
+    return round(duration, 2)
+
+api_key = 'AIzaSyCdkxrlZ8vXkr4BSAHJJIZKjkqVHDQvlBE'
+gmaps = googlemaps.Client(key=api_key)
+
+origin = "Roseville, CA"
+destination = "Marriott Marquis, Mission Street, San Francisco"
+arrival_date = '03/27/2018'
+month, day, year = map(int, arrival_date.split('/'))
+arrival_time = '12:00'
+hour, minute = map(int, arrival_time.split(':'))
+desired_arrival = datetime(year, month, day, hour, minute)
+
+# Estimate travel duration by getting directions with a departure time equal to the desired arrival time
+directions = get_directions(origin, destination, desired_arrival)
+estimated_duration = get_duration(directions, desired_arrival)
+estimated_departure = desired_arrival - timedelta(seconds=estimated_duration)
+
+# Determine the estimated departure time that produces the desired arrival time
+while True:
+    directions = get_directions(origin, destination, estimated_departure)    
+    delta = timedelta(seconds=get_duration(directions, estimated_departure))
+    estimated_arrival = estimated_departure + delta
+    if estimated_arrival <= desired_arrival:
+        break
+    else:
+        estimated_departure = estimated_departure - (estimated_arrival - desired_arrival)
+
+print(f'Estimated Departure: {estimated_departure.hour}:{estimated_departure.minute:02d}')
+print(f'Estimated Arrival: {estimated_arrival.hour}:{estimated_arrival.minute:02d}')
+print(f'Travel Time: {directions[0]["legs"][0]["duration"]["text"]}')
+print(f'Distance: {directions[0]["legs"][0]["distance"]["text"]}')
